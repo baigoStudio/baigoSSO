0e22d020d1863b7a2fb1f62d95cd1809aab18d4d
fone <fone@huangyifengdeMac-Pro.local>
2015-05-22 13:48:59 +0800

    v1.0.7

---
+++ core/lang/zh_CN/help/upgrade/config.php
@@ -0,0 +1,15 @@
+<?php
+return array(
+	"title" => "升级方法",
+	"menu" => array(
+		"outline"     => "升级概述",
+		"ext"         => "服务器环境检查",
+		"dbconfig"    => "数据库设置",
+		"dbtable"     => "升级数据库",
+		"base"        => "基本设置",
+		"reg"         => "注册设置",
+		"over"        => "完成安装",
+	),
+);
+
+
